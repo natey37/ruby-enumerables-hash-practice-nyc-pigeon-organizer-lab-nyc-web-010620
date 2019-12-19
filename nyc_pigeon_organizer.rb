06da7005d7249694d4fadddd4d3bd3d9c2ca9afa
Nathan Brickett <natey37@gmail.com>
2019-12-19 23:42:55 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -18,14 +18,14 @@ def nyc_pigeon_organizer(pigeon_info)
   pigeon_info[:gender].each do |gender, names|
     names.each do |name|
       if p_names.include?(name)
-        final[name][:gender] = gender.to_s
+        final[name][:gender] << gender.to_s
       end 
     end 
   end 
   pigeon_info[:lives].each do |place, names|
     names.each do |name|
       if p_names.include?(name)
-        final[name][:lives] = place 
+        final[name][:lives] << place 
       end 
     end 
   end 
