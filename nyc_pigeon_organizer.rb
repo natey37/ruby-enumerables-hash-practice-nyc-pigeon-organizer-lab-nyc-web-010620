94b78413cb6bb257e450afc6aa9634283cb79684
Nathan Brickett <natey37@gmail.com>
2019-12-19 23:43:10 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,35 @@
-def nyc_pigeon_organizer(data)
-  # write your code here!
-end
+def nyc_pigeon_organizer(pigeon_info)
+  final = {}
+  pigeon_info.each do |outer_info, inner_info|
+    inner_info.each do |category, array|
+      array.each do |name|
+      final[name] = { :color => [], :gender => [], :lives => []}
+      end 
+    end 
+  end 
+  p_names = final.keys
+  pigeon_info[:color].each do |bird_color, names|
+    names.each do |name|
+      if p_names.include?(name)
+        final[name][:color] << bird_color.to_s
+      end 
+    end 
+  end 
+  pigeon_info[:gender].each do |gender, names|
+    names.each do |name|
+      if p_names.include?(name)
+        final[name][:gender] << gender.to_s
+      end 
+    end 
+  end 
+  pigeon_info[:lives].each do |place, names|
+    names.each do |name|
+      if p_names.include?(name)
+        final[name][:lives] << place 
+      end 
+    end 
+  end 
+  final
+
+end 
+
